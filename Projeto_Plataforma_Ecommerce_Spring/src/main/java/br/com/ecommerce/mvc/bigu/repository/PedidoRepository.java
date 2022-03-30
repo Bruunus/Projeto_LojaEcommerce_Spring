f6397d1790b7ffdd0e7479e6aa653d5910e71c9e
BRUNO FERNANDES DOS SANTOS <brunofernandes@santamarcelina.org>
2022-03-30 18:15:44 -0300

    Adicionado novo método com query personalizada para busca no banco por usuário - método: findByUsuario

--- Projeto_Plataforma_Ecommerce_Spring/src/main/java/br/com/ecommerce/mvc/bigu/repository/PedidoRepository.java
+++ Projeto_Plataforma_Ecommerce_Spring/src/main/java/br/com/ecommerce/mvc/bigu/repository/PedidoRepository.java
@@ -3,6 +3,8 @@ package br.com.ecommerce.mvc.bigu.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.ecommerce.mvc.bigu.model.Pedido;
@@ -14,6 +16,16 @@ public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
 
 	List<Pedido> findByStatus(StatusPedido status);
+
+	/**
+	 * Método que realiza consulta no banco de dados através da @query especificada.
+	 * No caso para trazer os pedidos de acordo com o username especificado fazendo 
+	 * junção com a tabela 'users'.
+	 * @param username
+	 * @return
+	 */
+	@Query("SELECT p FROM Pedido p JOIN p.user u WHERE u.username = :username")
+	List<Pedido> findByUsuario(@Param("username")String username);
 	
 
 }
