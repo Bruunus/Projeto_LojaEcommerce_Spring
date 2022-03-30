68f364d898e667821f56dec5d6015f71944e9a38
BRUNO FERNANDES DOS SANTOS <brunofernandes@santamarcelina.org>
2022-03-30 18:06:13 -0300

    Feito ajuste de redirecionamento de página de login; Adicionado novo método para auteticação via JDBC e encriptação de senha de usuário.

--- Projeto_Plataforma_Ecommerce_Spring/src/main/java/br/com/ecommerce/mvc/bigu/WebSecurityConfig.java
+++ Projeto_Plataforma_Ecommerce_Spring/src/main/java/br/com/ecommerce/mvc/bigu/WebSecurityConfig.java
@@ -1,13 +1,17 @@
 package br.com.ecommerce.mvc.bigu;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
@@ -15,6 +19,10 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	
+	private DataSource dataSource;
+	
+	
 	
 	/**
 	 * Método spring de configuração de autorizção de usuário
@@ -28,29 +36,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 				.formLogin(form -> form
 		            .loginPage("/login")
+		            .defaultSuccessUrl("/home", true)
 		            .permitAll()
-		        );
+		        )
+			.logout(logout -> logout.logoutUrl("/logout"))
+			.csrf().disable();
+			
 	}
 	
 	
-	/**
-	 * Método spring de autenticação de usuário.
+	/*
+	 * Método proposital para informar ao spring security que as autenticações
+	 * serão feitas através do JDBC. Criado um objeto BCryptPasswordEncoder
+	 * para criptografar as senhas no banco de dados, utilizando o recurso
+	 * UserDetails para já subir a aplicação criando um usuário no bando e já 
+	 * com senha criptografada e configuração da AuthenticationManagerBuilder
+	 * do objeto auth que realiza as configurações de acesso ao bando via
+	 * JDBC e configurações de encriptação de senhas.
 	 */
-	@Bean
 	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("brunus")
-				.password("brunus")
-				.roles("ADM")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	
+		auth
+		.jdbcAuthentication()
+		.dataSource(dataSource)
+		.passwordEncoder(encoder);
+ 
 	}
-	
-	
-	
 	
 	
 	
