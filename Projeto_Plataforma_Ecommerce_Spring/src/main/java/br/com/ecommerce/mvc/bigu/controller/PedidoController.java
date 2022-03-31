2af49b55fe2953a49aa2adc6e7066b46de8fb461
BRUNO FERNANDES DOS SANTOS <brunofernandes@santamarcelina.org>
2022-03-31 17:21:31 -0300

    Adicionado nova funcionalidade no controller PedidoController para mapear o usuário autenticado e vínculá-lo à sua conta no banco de dados utilizando a interface 'UserRepository'. Tambem retirado comentários desnecessários.

--- Projeto_Plataforma_Ecommerce_Spring/src/main/java/br/com/ecommerce/mvc/bigu/controller/PedidoController.java
+++ Projeto_Plataforma_Ecommerce_Spring/src/main/java/br/com/ecommerce/mvc/bigu/controller/PedidoController.java
@@ -1,9 +1,7 @@
 package br.com.ecommerce.mvc.bigu.controller;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -13,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.ecommerce.mvc.bigu.dto.RequisicaoNovoPedido;
 import br.com.ecommerce.mvc.bigu.model.Pedido;
+import br.com.ecommerce.mvc.bigu.model.User;
 import br.com.ecommerce.mvc.bigu.repository.PedidoRepository;
+import br.com.ecommerce.mvc.bigu.repository.UserRepository;
 
 
 @Controller @RequestMapping("pedido")
@@ -21,7 +21,11 @@ public class PedidoController {
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
-
+	@Autowired
+	private UserRepository userRepository;
+	
+	
+	
 	
 	@GetMapping("/formulario")
 	public String formulario(RequisicaoNovoPedido request) {
@@ -30,6 +34,14 @@ public class PedidoController {
 	
 	
 	/**
+	 * Método de requisição para cadastrar um produto no banco de dados. Recebe um RequisicaoNovoPedido
+	 * Contendo um filtro de validação de preenchimento e um BingingResult para tratativa de erro.
+	 * SecurityContextHolder guardada em uma variável para descobrir o nome do usuário que está logado
+	 * Criado uma query de select para identificar o usuário pelo nome através do repository UserRepository
+	 * Instanciado um objeto do tipo pedido em variável que chamará o método setter e passando o nome do usuário 
+	 * recuperado para associa-lo entre o banco e a aplicação. Por fim realiza o cadastro do ítem vinculado ao
+	 * usuário.
+	 * 
 	 * @valid - O Spring se conecta com a notação @NotBlank de validação dos atributos para entrada de dados deste parâmetro
 	 * @param request
 	 * @return - O objeto do parâmetro BindingResult informa o resultado da validação dos dados acionando um erro ou não.
@@ -40,10 +52,14 @@ public class PedidoController {
 		if(result.hasErrors())		
 			return "pedido/formulario";
 		
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		User user = userRepository.findByUsername(username);
+		
 		Pedido pedido = request.toPedido();
+		pedido.setUser(user);
 		pedidoRepository.save(pedido);
 		
-		return "redirect:/home";  // ou     return "forward:/home"; - serve também
+		return "redirect:/home";   
 	}
 	
 }
