8159d9b25257226789f851be6e1f9dc2098362e5
BRUNO FERNANDES DOS SANTOS <brunofernandes@santamarcelina.org>
2022-04-06 17:27:21 -0300

    Modificação no arquivo HomeController - Implantado nova configuração  de ordenação de pedidos e paginação para a home.html (pública). Modificação no método de busca do bando de dados, inserido novo parâmetro de paginação

--- Projeto_Plataforma_Ecommerce_Spring/src/main/java/br/com/ecommerce/mvc/bigu/controller/HomeController.java
+++ Projeto_Plataforma_Ecommerce_Spring/src/main/java/br/com/ecommerce/mvc/bigu/controller/HomeController.java
@@ -3,14 +3,12 @@ package br.com.ecommerce.mvc.bigu.controller;
 import java.security.Principal;
 import java.util.List;
 
-import javax.persistence.PersistenceContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.ecommerce.mvc.bigu.model.Pedido;
@@ -31,7 +29,9 @@ public class HomeController  {
 	 * Método que recebe requisição GET	com prefixo home.
 	 * Estabelece conexão direta com banco de dados executando
 	 * consulta personalizada para carregar a lista de todos
-	 * os pedidos. injetar o nome do usuário em que está 
+	 * os pedidos usando paginação com PageRequest (qtd de páginas, 
+	 * qtd de ítens na página) e em ordem da data do pedido 
+	 * utilizando o sort. Injetar o nome do usuário em que está 
 	 * logado na sessão atual. O model na qual trabalha 
 	 * com UI (Interface de Usuário) chama o despachador 
 	 * addAtributte informando um nome e passando o objeto 
@@ -43,46 +43,14 @@ public class HomeController  {
 	 */
 	@GetMapping
 	public String home(Model model, Principal principal) {		
-		List<Pedido> pedidos = pedidoRepository.findAll();		// conexão com banco de dados
+		Sort sort = Sort.by("dataDaEntrega").descending();
+		PageRequest paginacao = PageRequest.of(0, 5, sort);
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao);		// conexão com banco de dados
 		model.addAttribute("pedidos", pedidos);	
 		return "home";
 	}
 	
 
-	
-	/**
-	 * Controller redirecionador para links internos da página home.
-	 * Este método é chamado dependendo da URL que for chamada, e ao ser 
-	 * invocado realiza uma busca no banco de dados para listar os
-	 * dados. A referência para o valor no @GetMapping é aquele que
-	 * recebe o nome de status, e vem através do request. Na assinatura 
-	 * do método foi especificado pelo spring que seja adicionado na 
-	 * variável status vinda da URL da request própriamente na variável 
-	 * String status.
-	 * 
-	 * 
-	 * @param model
-	 * @return
-	 */
-	@GetMapping("/{status}")
-	public String porStatus(@PathVariable("status")String status, Model model) {		
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
-		model.addAttribute("pedidos", pedidos);	
-		model.addAttribute("status", status);
-		return "home";
-	}
-	
-	
-	
-	/**
-	 * Tratamento de erro do método 'porStatus' caso ele não consiga atender a requisição.
-	 * @return - Página home da aplicação
-	 */
-	@ExceptionHandler(IllegalArgumentException.class)
-	public String onError() {
-		return "redirect:/home";
-	}
-	
 	
 	
 }
